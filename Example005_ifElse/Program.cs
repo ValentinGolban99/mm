47111cbf7408b5990252ce2d9036de881cba420e
Valentin <val.golban2014@yandex.ru>
2022-08-21 16:35:25 +0300

    Разобрали команды if else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")//ToLower - для решения проблемы большых букв. if - если.
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else//иначе
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
+
+
