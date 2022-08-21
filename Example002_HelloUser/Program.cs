19c3eb6f71a0976b1fda7a7ea300a9b1b2e7b5fb
Valentin <val.golban2014@yandex.ru>
2022-08-21 15:29:24 +0300

    Создал коммит Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Хранилище для переменной
+//string - строка, username - название переменной
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+// Приветствие в консоль
+Console.Write("Привет, ");
+Console.Write(username);
