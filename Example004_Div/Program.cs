08f29d338ef295d4304b1fafd477cf048d4f0a25
Valentin <val.golban2014@yandex.ru>
2022-08-21 16:20:14 +0300

    Добавил рандом в Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,7 @@
 ﻿//int - целое, double - с дробью
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);;
+Console.WriteLine(numberB);
+int resulte = (numberA + numberB);
+Console.WriteLine(resulte);
